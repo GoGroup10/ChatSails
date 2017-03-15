235a878e6b3c54c3dcfd8f61f7c51f198108d85e
warrenchulls <warren.as123@gmail.com>
2017-03-15 11:29:29 -0500

    cru

--- ChatApp/config/models.js
+++ ChatApp/config/models.js
@@ -9,7 +9,6 @@
  * http://sailsjs.org/#!/documentation/concepts/ORM
  */
 module.exports.models = {
-<<<<<<< HEAD
     /***************************************************************************
      *                                                                          *
      * Your app's default connection. i.e. the name of one of your app's        *
@@ -27,25 +26,3 @@ module.exports.models = {
      ***************************************************************************/
     migrate: 'alter'
 };
-=======
-
-  /***************************************************************************
-  *                                                                          *
-  * Your app's default connection. i.e. the name of one of your app's        *
-  * connections (see `config/connections.js`)                                *
-  *                                                                          *
-  ***************************************************************************/
-   connection: 'Mongo',
-
-  /***************************************************************************
-  *                                                                          *
-  * How and whether Sails will attempt to automatically rebuild the          *
-  * tables/collections/etc. in your schema.                                  *
-  *                                                                          *
-  * See http://sailsjs.org/#!/documentation/concepts/ORM/model-settings.html  *
-  *                                                                          *
-  ***************************************************************************/
-  migrate: 'alter'
-
-};
->>>>>>> ed77cd1ea9545ab4121e6067d9482e5bb4c73a89
